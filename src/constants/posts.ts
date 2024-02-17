336c3871fd4a1c625848df3ff8d95a1bde4a8390
eliot-liner <eliot@linercorp.com>
2024-02-17 20:43:17 +0900

    commit

--- src/constants/posts.ts
+++ src/constants/posts.ts
@@ -1,7 +1,7 @@
 export const posts = [
   {
-    id: "nickname4",
-    lastModified: "2024-02-17T11:23:00.000Z",
+    id: "nickname2",
+    lastModified: "2024-02-17T11:37:00.000Z",
     description:
       "게임에서 눈에 띄는 최고의 닉네임 추천. 다양한 게임 캐릭터와 스타일에 맞는 독특하고 기억에 남는 닉네임을 찾아보세요.",
     title: "게임에서 눈에 띄는 최고의 닉네임 정하는 방법",
@@ -9,10 +9,20 @@ export const posts = [
       "https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F92d37f21-1548-474e-a4bb-a1e63f34174e%2F738e6f4d-05fd-4171-8669-ec0995093994%2FUntitled.png?table=block&id=8b6bcc2e-1b14-42b9-84bc-07fa79385633&spaceId=92d37f21-1548-474e-a4bb-a1e63f34174e&width=2000&userId=4ea46ae5-b051-4e61-b69c-6bbde16eb9ab&cache=v2",
     content:
       "\n![main.f5bb7950.png](https://ranking-place.com/_next/static/media/main.f5bb7950.png)\n\n\n게임 세계 속에서 닉네임은 단순한 명칭 이상의 의미를 지닙니다. 그것은 당신의 캐릭터, 스타일, 심지어 게임 내에서의 정체성까지 표현하는 열쇠이죠. 전사에서 마법사, 신비한 생물에 이르기까지, 각각의 역할에 걸맞은 독특하고 기억에 남는 닉네임을 선택하는 것은 게임 내에서의 당신만의 존재감을 드러내고, 다른 플레이어들과의 소통을 향상시키는 중요한 방법입니다. 2023년, 게임에서 당신만의 정체성을 찾아 나서는 이 여정에서, 이 닉네임들이 당신을 더욱 돋보이게 할 것입니다. 다양한 게임 장르와 선호도에 맞춰 제공되는 이 닉네임들로 게임 세계에서 당신의 이야기를 만들어보세요.\n\n\n## **재미와 유머를 강조한 닉네임:**\n\n\n게임의 세계는 때로는 진지함보다 웃음과 즐거움을 선사하는 곳이죠. 그렇기 때문에, 유머가 담긴 닉네임은 게임 내에서 당신을 더욱 돋보이게 만들어요. 예를 들어, '백설공주'에서 영감을 받아 '뱃살공주'로, 혹은 '짱구는 못말려'를 '짱구는 목말려'로 바꿔, 게임 속에서 유쾌한 분위기의 주인공이 될 수 있어요. 이런 닉네임은 게임을 하는 동안 당신의 존재를 더욱 빛나게 해주며, 다른 플레이어들과의 교류에서도 긍정적인 인상을 남길 거예요.\n\n\n## **커플 닉네임**:\n\n\n게임을 더욱 특별하게 만들고 싶은 커플이라면, 서로를 연결짓는 닉네임을 선택하는 것이 좋아요. 예를 들어, '달과별', '해와달'과 같이 서로의 존재를 상징하는 닉네임은 게임 속에서 여러분의 관계를 더욱 돋보이게 할 거예요. 게임 속에서 여러분만의 사랑을 표현하며, 다른 플레이어들에게도 잊을 수 없는 인상을 남길 수 있겠죠.\n\n\n## **두 글자 닉네임**:\n\n\n간결함 속에서도 강렬한 인상을 남기고 싶다면, 두 글자 닉네임을 선택하는 것이 좋습니다. '천사', '바람' 같은 단어는 기억하기 쉬우면서도 강한 인상을 남겨, 게임 내에서 당신을 쉽게 식별할 수 있게 해줘요. 이런 닉네임은 게임 속에서 당신의 캐릭터를 더욱 독특하게 만들어주며, 당신의 게임 스타일을 반영할 수 있어요.\n\n\n## **당신만의 이야기를 담은 닉네임:**\n\n\n 자신만의 취향, 성격, 게임 속 역할을 반영한 닉네임은 당신이 누구인지, 무엇을 좋아하는지를 세상에 알리는 방법이에요. 예를 들어, '밤하늘의별'은 조용히 밤하늘을 바라보는 것을 좋아하는 당신의 성향을, '화염의마법사'는 게임 속에서 마법사 캐릭터를 선호하는 당신의 취향을 반영할 수 있어요.\n\n\n## **암호화된 메시지:**\n\n\n 자신만이 알 수 있는 숨겨진 의미나 메시지를 닉네임에 포함시키는 것도 재미있는 방법이에요. 예를 들어, 중요한 날짜나 숫자, 기호를 혼합하여 'X7J24'나 'N3oStar' 같은 닉네임을 만들 수 있죠.\n\n\n## **자연 요소:**\n\n\n 자연에서 영감을 받은 닉네임도 멋진 선택이 될 수 있어요. '폭풍의속삭임', '바다의왕자'와 같이 자연의 웅장함과 아름다움을 반영하는 이름들이죠.\n\n\n## **개인화된 닉네임:**\n\n\n당신만의 독특한 닉네임을 원한다면, 자신의 취미, 성격, 혹은 게임 내에서 선호하는 역할을 반영한 닉네임을 고려해보세요. 이 방법은 당신의 게임 내 캐릭터를 더욱 개성있게 만들어 줄 거예요. 예를 들어, 당신이 게임 내에서 전사를 주로 선택한다면, '전투의신 정상수'이라는 닉네임을 선택할 수 있겠죠. 당신의 게임 플레이 스타일과 성격을 반영한 닉네임은 게임 속에서 당신을 더욱 독특하게 만들어 줄 것입니다.\n\n\n이렇게 닉네임을 고르는 과정은 당신의 게임 내 존재를 정의하는 중요한 단계에요. 자신만의 개성과 스타일을 반영한 닉네임을 선택하여, 게임 세계 속에서 독특한 정체성을 구축해보세요!\n\n",
+  },
+  {
+    id: "protein ",
+    lastModified: "2024-02-17T11:37:00.000Z",
+    description: "단백질 파우더 순위를 정하고 각 파우더의 특징을 분석합니다.",
+    title: "2024 단백질 보충제 추천",
+    image:
+      "https://www.notion.so/image/https%3A%2F%2Franking-place.com%2F_next%2Fstatic%2Fmedia%2F1.26a11e23.webp?table=block&id=2c1cde19-948f-4f12-89e8-f2b55b15dd95&spaceId=92d37f21-1548-474e-a4bb-a1e63f34174e&width=2000&userId=4ea46ae5-b051-4e61-b69c-6bbde16eb9ab&cache=v2",
+    content:
+      "\n운동, 다이어트 할 때 단백질 보충제를 찾을 겁니다.\n더 많은 단백질과 적은 탄수화물로 체중이나 근육을 늘릴 수 있습니다.\n가장 좋은 단백질 보충제들의 순위를 매기고 정리했습니다\n\n\n## **#1 비에스엔 신타-6 프로틴 파우더 드링크 믹스 단백질 보충제 2.27kg**\n\n\n![1.26a11e23.webp](https://ranking-place.com/_next/static/media/1.26a11e23.webp)\n\n\n가격: 3, 8.3만원\n\n\n맛: 5, 초콜릿맛\n\n\n식사대용: 2, 헬스용\n\n\n유당 제거: 5, 제거됨\n\n\n헬스인들에게 유명한 단백질 보충제입니다.\n맛은 굉장히 좋고 유당이 제거되 복통같은 부작용은 없습니다\n\n\n## **#2 머슬팜 컴뱃 울트라 웨이 프로틴 파우더 드링크 믹스 단백질 보충제 초콜릿 밀크 2269g**\n\n\n![2.9c8e33c6.webp](https://ranking-place.com/_next/static/media/2.9c8e33c6.webp)\n\n\n가격: 3, 9.3만원\n\n\n맛: 5, 초콜릿맛\n\n\n식사대용: 1, 헬스용\n\n\n유당 제거: 5, 제거됨\n\n\n신타6과 비슷하지만 헬스인에게 더 초점을 맞춘 제품입니다.\n지방이 더 적어지고 약간의 단백질이 늘어났으며 운동 후 흡수를 도와주는 보출제입니다.\n\n\n## **#3 원데이뉴트리션 머슬킹 80 프로틴 헬스 보충제**\n\n\n![3.9baadf37.webp](https://ranking-place.com/_next/static/media/3.9baadf37.webp)\n\n\n가격: 5, 7.1만원\n\n\n맛: 3, 초콜릿+곡물맛\n\n\n식사대용: 4, 식사대용가능\n\n\n유당 제거: 3, 약간 함유\n\n\n가성비가 좋은 보충제입니다. 할인가를 많이 해 절반 가격에 구매하실 수 있습니다.\n흡수하는 데 오래 걸리는 단백질이라 식사대용으로 많이 사용합니다.\n다만 약간의 코코아향과 곡물 맛으로 고소한 맛으로 먹는 제품입니다.\n\n\n## **#4 칼로바이 퍼펙트 파워쉐이크 초코맛 단백질보충제 2kg**\n\n\n![4.61e584b1.webp](https://ranking-place.com/_next/static/media/4.61e584b1.webp)\n\n\n가격: 4, 8.4만원\n\n\n맛: 4, 초콜릿맛\n\n\n식사대용: 4, 식사대용가능\n\n\n유당 제거: 3, 약간 함유\n\n\n위 제품의 중간격인 제품으로\n가성비, 맛, 식사대용, 등 다양한 상황에서 호불호 없이 먹을 수 있습니다.\n\n",
   },
   {
     id: "nickname3",
-    lastModified: "2024-02-17T11:23:00.000Z",
+    lastModified: "2024-02-17T11:37:00.000Z",
     description:
       "게임에서 눈에 띄는 최고의 닉네임 추천. 다양한 게임 캐릭터와 스타일에 맞는 독특하고 기억에 남는 닉네임을 찾아보세요.",
     title: "게임에서 눈에 띄는 최고의 닉네임 정하는 방법",
@@ -22,8 +32,28 @@ export const posts = [
       "\n![main.f5bb7950.png](https://ranking-place.com/_next/static/media/main.f5bb7950.png)\n\n\n게임 세계 속에서 닉네임은 단순한 명칭 이상의 의미를 지닙니다. 그것은 당신의 캐릭터, 스타일, 심지어 게임 내에서의 정체성까지 표현하는 열쇠이죠. 전사에서 마법사, 신비한 생물에 이르기까지, 각각의 역할에 걸맞은 독특하고 기억에 남는 닉네임을 선택하는 것은 게임 내에서의 당신만의 존재감을 드러내고, 다른 플레이어들과의 소통을 향상시키는 중요한 방법입니다. 2023년, 게임에서 당신만의 정체성을 찾아 나서는 이 여정에서, 이 닉네임들이 당신을 더욱 돋보이게 할 것입니다. 다양한 게임 장르와 선호도에 맞춰 제공되는 이 닉네임들로 게임 세계에서 당신의 이야기를 만들어보세요.\n\n\n## **재미와 유머를 강조한 닉네임:**\n\n\n게임의 세계는 때로는 진지함보다 웃음과 즐거움을 선사하는 곳이죠. 그렇기 때문에, 유머가 담긴 닉네임은 게임 내에서 당신을 더욱 돋보이게 만들어요. 예를 들어, '백설공주'에서 영감을 받아 '뱃살공주'로, 혹은 '짱구는 못말려'를 '짱구는 목말려'로 바꿔, 게임 속에서 유쾌한 분위기의 주인공이 될 수 있어요. 이런 닉네임은 게임을 하는 동안 당신의 존재를 더욱 빛나게 해주며, 다른 플레이어들과의 교류에서도 긍정적인 인상을 남길 거예요.\n\n\n## **커플 닉네임**:\n\n\n게임을 더욱 특별하게 만들고 싶은 커플이라면, 서로를 연결짓는 닉네임을 선택하는 것이 좋아요. 예를 들어, '달과별', '해와달'과 같이 서로의 존재를 상징하는 닉네임은 게임 속에서 여러분의 관계를 더욱 돋보이게 할 거예요. 게임 속에서 여러분만의 사랑을 표현하며, 다른 플레이어들에게도 잊을 수 없는 인상을 남길 수 있겠죠.\n\n\n## **두 글자 닉네임**:\n\n\n간결함 속에서도 강렬한 인상을 남기고 싶다면, 두 글자 닉네임을 선택하는 것이 좋습니다. '천사', '바람' 같은 단어는 기억하기 쉬우면서도 강한 인상을 남겨, 게임 내에서 당신을 쉽게 식별할 수 있게 해줘요. 이런 닉네임은 게임 속에서 당신의 캐릭터를 더욱 독특하게 만들어주며, 당신의 게임 스타일을 반영할 수 있어요.\n\n\n## **당신만의 이야기를 담은 닉네임:**\n\n\n 자신만의 취향, 성격, 게임 속 역할을 반영한 닉네임은 당신이 누구인지, 무엇을 좋아하는지를 세상에 알리는 방법이에요. 예를 들어, '밤하늘의별'은 조용히 밤하늘을 바라보는 것을 좋아하는 당신의 성향을, '화염의마법사'는 게임 속에서 마법사 캐릭터를 선호하는 당신의 취향을 반영할 수 있어요.\n\n\n## **암호화된 메시지:**\n\n\n 자신만이 알 수 있는 숨겨진 의미나 메시지를 닉네임에 포함시키는 것도 재미있는 방법이에요. 예를 들어, 중요한 날짜나 숫자, 기호를 혼합하여 'X7J24'나 'N3oStar' 같은 닉네임을 만들 수 있죠.\n\n\n## **자연 요소:**\n\n\n 자연에서 영감을 받은 닉네임도 멋진 선택이 될 수 있어요. '폭풍의속삭임', '바다의왕자'와 같이 자연의 웅장함과 아름다움을 반영하는 이름들이죠.\n\n\n## **개인화된 닉네임:**\n\n\n당신만의 독특한 닉네임을 원한다면, 자신의 취미, 성격, 혹은 게임 내에서 선호하는 역할을 반영한 닉네임을 고려해보세요. 이 방법은 당신의 게임 내 캐릭터를 더욱 개성있게 만들어 줄 거예요. 예를 들어, 당신이 게임 내에서 전사를 주로 선택한다면, '전투의신 정상수'이라는 닉네임을 선택할 수 있겠죠. 당신의 게임 플레이 스타일과 성격을 반영한 닉네임은 게임 속에서 당신을 더욱 독특하게 만들어 줄 것입니다.\n\n\n이렇게 닉네임을 고르는 과정은 당신의 게임 내 존재를 정의하는 중요한 단계에요. 자신만의 개성과 스타일을 반영한 닉네임을 선택하여, 게임 세계 속에서 독특한 정체성을 구축해보세요!\n\n",
   },
   {
-    id: "nickname2",
-    lastModified: "2024-02-17T11:23:00.000Z",
+    id: "nickname",
+    lastModified: "2024-02-17T11:37:00.000Z",
+    description:
+      "게임에서 눈에 띄는 최고의 닉네임 추천. 다양한 게임 캐릭터와 스타일에 맞는 독특하고 기억에 남는 닉네임을 찾아보세요.",
+    title: "게임에서 눈에 띄는 최고의 닉네임 정하는 방법",
+    image:
+      "https://dust-satin-8b0.notion.site/image/https%3A%2F%2Franking-place.com%2F_next%2Fstatic%2Fmedia%2Fmain.f5bb7950.png?table=block&id=1713a9cb-c73e-4d70-a07a-d45dfb4747c1&spaceId=92d37f21-1548-474e-a4bb-a1e63f34174e&width=2000&userId=&cache=v2",
+    content:
+      "\n![main.f5bb7950.png](https://ranking-place.com/_next/static/media/main.f5bb7950.png)\n\n\n게임 세계 속에서 닉네임은 단순한 명칭 이상의 의미를 지닙니다. 그것은 당신의 캐릭터, 스타일, 심지어 게임 내에서의 정체성까지 표현하는 열쇠이죠. 전사에서 마법사, 신비한 생물에 이르기까지, 각각의 역할에 걸맞은 독특하고 기억에 남는 닉네임을 선택하는 것은 게임 내에서의 당신만의 존재감을 드러내고, 다른 플레이어들과의 소통을 향상시키는 중요한 방법입니다. 2023년, 게임에서 당신만의 정체성을 찾아 나서는 이 여정에서, 이 닉네임들이 당신을 더욱 돋보이게 할 것입니다. 다양한 게임 장르와 선호도에 맞춰 제공되는 이 닉네임들로 게임 세계에서 당신의 이야기를 만들어보세요.\n\n\n## **재미와 유머를 강조한 닉네임:**\n\n\n게임의 세계는 때로는 진지함보다 웃음과 즐거움을 선사하는 곳이죠. 그렇기 때문에, 유머가 담긴 닉네임은 게임 내에서 당신을 더욱 돋보이게 만들어요. 예를 들어, '백설공주'에서 영감을 받아 '뱃살공주'로, 혹은 '짱구는 못말려'를 '짱구는 목말려'로 바꿔, 게임 속에서 유쾌한 분위기의 주인공이 될 수 있어요. 이런 닉네임은 게임을 하는 동안 당신의 존재를 더욱 빛나게 해주며, 다른 플레이어들과의 교류에서도 긍정적인 인상을 남길 거예요.\n\n\n## **커플 닉네임**:\n\n\n게임을 더욱 특별하게 만들고 싶은 커플이라면, 서로를 연결짓는 닉네임을 선택하는 것이 좋아요. 예를 들어, '달과별', '해와달'과 같이 서로의 존재를 상징하는 닉네임은 게임 속에서 여러분의 관계를 더욱 돋보이게 할 거예요. 게임 속에서 여러분만의 사랑을 표현하며, 다른 플레이어들에게도 잊을 수 없는 인상을 남길 수 있겠죠.\n\n\n## **두 글자 닉네임**:\n\n\n간결함 속에서도 강렬한 인상을 남기고 싶다면, 두 글자 닉네임을 선택하는 것이 좋습니다. '천사', '바람' 같은 단어는 기억하기 쉬우면서도 강한 인상을 남겨, 게임 내에서 당신을 쉽게 식별할 수 있게 해줘요. 이런 닉네임은 게임 속에서 당신의 캐릭터를 더욱 독특하게 만들어주며, 당신의 게임 스타일을 반영할 수 있어요.\n\n\n## **당신만의 이야기를 담은 닉네임:**\n\n\n 자신만의 취향, 성격, 게임 속 역할을 반영한 닉네임은 당신이 누구인지, 무엇을 좋아하는지를 세상에 알리는 방법이에요. 예를 들어, '밤하늘의별'은 조용히 밤하늘을 바라보는 것을 좋아하는 당신의 성향을, '화염의마법사'는 게임 속에서 마법사 캐릭터를 선호하는 당신의 취향을 반영할 수 있어요.\n\n\n## **암호화된 메시지:**\n\n\n 자신만이 알 수 있는 숨겨진 의미나 메시지를 닉네임에 포함시키는 것도 재미있는 방법이에요. 예를 들어, 중요한 날짜나 숫자, 기호를 혼합하여 'X7J24'나 'N3oStar' 같은 닉네임을 만들 수 있죠.\n\n\n## **자연 요소:**\n\n\n 자연에서 영감을 받은 닉네임도 멋진 선택이 될 수 있어요. '폭풍의속삭임', '바다의왕자'와 같이 자연의 웅장함과 아름다움을 반영하는 이름들이죠.\n\n\n## **개인화된 닉네임:**\n\n\n당신만의 독특한 닉네임을 원한다면, 자신의 취미, 성격, 혹은 게임 내에서 선호하는 역할을 반영한 닉네임을 고려해보세요. 이 방법은 당신의 게임 내 캐릭터를 더욱 개성있게 만들어 줄 거예요. 예를 들어, 당신이 게임 내에서 전사를 주로 선택한다면, '전투의신 정상수'이라는 닉네임을 선택할 수 있겠죠. 당신의 게임 플레이 스타일과 성격을 반영한 닉네임은 게임 속에서 당신을 더욱 독특하게 만들어 줄 것입니다.\n\n\n이렇게 닉네임을 고르는 과정은 당신의 게임 내 존재를 정의하는 중요한 단계에요. 자신만의 개성과 스타일을 반영한 닉네임을 선택하여, 게임 세계 속에서 독특한 정체성을 구축해보세요!\n\n",
+  },
+  {
+    id: "christmas-place",
+    lastModified: "2024-02-17T11:43:00.000Z",
+    description:
+      "크리스마스에 완벽한 여행지를 찾고 계신가요? 세계 곳곳의 크리스마스 명소를 이 가이드를 통해 비교하세요.",
+    title: "크리스마스를 더욱 특별하게 만들어 줄 최고의 여행지",
+    image: "",
+    content: "\nexe\n\n",
+  },
+  {
+    id: "nickname4",
+    lastModified: "2024-02-17T11:37:00.000Z",
     description:
       "게임에서 눈에 띄는 최고의 닉네임 추천. 다양한 게임 캐릭터와 스타일에 맞는 독특하고 기억에 남는 닉네임을 찾아보세요.",
     title: "게임에서 눈에 띄는 최고의 닉네임 정하는 방법",
@@ -34,35 +64,14 @@ export const posts = [
   },
   {
     id: "backpack",
-    lastModified: "2024-02-17T10:15:00.000Z",
+    lastModified: "2024-02-17T11:37:00.000Z",
     description:
-      "Here are our picks for the best backpacks in 2024, with detailed reviews. Compare backpacks for different uses and styles with this guide.",
-    title: "The best backpacks for 2024: a selection of uses and styles",
+      "추천하는 최고의 백팩들을 상세한 리뷰와 함께 소개합니다. 다양한 용도와 스타일의 백팩을 이 가이드를 통해 비교하세요.",
+    title: "추천 최고의 백팩: 다양한 용도와 스타일로 선별",
     image:
       "https://www.notion.so/image/https%3A%2F%2Franking-place.com%2F_next%2Fstatic%2Fmedia%2Fmain.be9e87a9.png?table=block&id=e27ddaaa-f5d8-493d-8797-921acc681bc4&spaceId=92d37f21-1548-474e-a4bb-a1e63f34174e&width=2000&userId=4ea46ae5-b051-4e61-b69c-6bbde16eb9ab&cache=v2",
     content:
       "\nIn 2023, we've curated a list of the best backpacks considering a variety of uses and styles. This selection is designed to meet the needs of everyone from students to professionals to travelers, evaluating each product on durability, design, storage space, and comfort. This guide will help you find the perfect backpack for your daily life, work, or travels.\n\n\n![1.6996384f.webp](https://ranking-place.com/_next/static/media/1.6996384f.webp)\n\n- **The North Face Borealis Backpack**: Boasting robust durability and multifunctionality, this backpack is made from sturdy materials for long-term use, offering various storage spaces and adjustable straps for comfortable wear. It's suitable for a wide range of users from students to professionals and comes in various colors.\n\n![2.7877533a.webp](https://ranking-place.com/_next/static/media/2.7877533a.webp)\n\n- **Herschel Little America Backpack**: Combining classic design with modern functionality, this backpack features a spacious main compartment and a laptop sleeve, making it ideal not only for daily use but also for short trips. Its stylish design is enhanced with various patterns.\n\n\t![3.189064fb.webp](https://ranking-place.com/_next/static/media/3.189064fb.webp)\n\n- **Osprey Farpoint 40 Travel Backpack**: Optimized for travel, this backpack is lightweight and durable, with ample storage space, making it suitable for long trips. It offers various adjustment features and back support for comfortable wear over extended periods.\n\n\t![4.1020fd87.webp](https://ranking-place.com/_next/static/media/4.1020fd87.webp)\n\n- **Fjallraven Kanken Backpack**: Loved for its iconic design and practicality, this backpack is made from waterproof Vinylon fabric, making it versatile for weather conditions, suitable for simple travels or daily use. It allows for personal expression with its variety of colors and patterns.\n",
-  },
-  {
-    id: "protein ",
-    lastModified: "2024-02-17T11:13:00.000Z",
-    description: "단백질 파우더 순위를 정하고 각 파우더의 특징을 분석합니다.",
-    title: "2024 단백질 보충제 추천",
-    image:
-      "https://www.notion.so/image/https%3A%2F%2Franking-place.com%2F_next%2Fstatic%2Fmedia%2F1.26a11e23.webp?table=block&id=2c1cde19-948f-4f12-89e8-f2b55b15dd95&spaceId=92d37f21-1548-474e-a4bb-a1e63f34174e&width=2000&userId=4ea46ae5-b051-4e61-b69c-6bbde16eb9ab&cache=v2",
-    content:
-      "\n운동, 다이어트 할 때 단백질 보충제를 찾을 겁니다.\n더 많은 단백질과 적은 탄수화물로 체중이나 근육을 늘릴 수 있습니다.\n가장 좋은 단백질 보충제들의 순위를 매기고 정리했습니다\n\n\n## **#1 비에스엔 신타-6 프로틴 파우더 드링크 믹스 단백질 보충제 2.27kg**\n\n\n![1.26a11e23.webp](https://ranking-place.com/_next/static/media/1.26a11e23.webp)\n\n\n가격: 3, 8.3만원\n\n\n맛: 5, 초콜릿맛\n\n\n식사대용: 2, 헬스용\n\n\n유당 제거: 5, 제거됨\n\n\n헬스인들에게 유명한 단백질 보충제입니다.\n맛은 굉장히 좋고 유당이 제거되 복통같은 부작용은 없습니다\n\n\n## **#2 머슬팜 컴뱃 울트라 웨이 프로틴 파우더 드링크 믹스 단백질 보충제 초콜릿 밀크 2269g**\n\n\n![2.9c8e33c6.webp](https://ranking-place.com/_next/static/media/2.9c8e33c6.webp)\n\n\n가격: 3, 9.3만원\n\n\n맛: 5, 초콜릿맛\n\n\n식사대용: 1, 헬스용\n\n\n유당 제거: 5, 제거됨\n\n\n신타6과 비슷하지만 헬스인에게 더 초점을 맞춘 제품입니다.\n지방이 더 적어지고 약간의 단백질이 늘어났으며 운동 후 흡수를 도와주는 보출제입니다.\n\n\n## **#3 원데이뉴트리션 머슬킹 80 프로틴 헬스 보충제**\n\n\n![3.9baadf37.webp](https://ranking-place.com/_next/static/media/3.9baadf37.webp)\n\n\n가격: 5, 7.1만원\n\n\n맛: 3, 초콜릿+곡물맛\n\n\n식사대용: 4, 식사대용가능\n\n\n유당 제거: 3, 약간 함유\n\n\n가성비가 좋은 보충제입니다. 할인가를 많이 해 절반 가격에 구매하실 수 있습니다.\n흡수하는 데 오래 걸리는 단백질이라 식사대용으로 많이 사용합니다.\n다만 약간의 코코아향과 곡물 맛으로 고소한 맛으로 먹는 제품입니다.\n\n\n## **#4 칼로바이 퍼펙트 파워쉐이크 초코맛 단백질보충제 2kg**\n\n\n![4.61e584b1.webp](https://ranking-place.com/_next/static/media/4.61e584b1.webp)\n\n\n가격: 4, 8.4만원\n\n\n맛: 4, 초콜릿맛\n\n\n식사대용: 4, 식사대용가능\n\n\n유당 제거: 3, 약간 함유\n\n\n위 제품의 중간격인 제품으로\n가성비, 맛, 식사대용, 등 다양한 상황에서 호불호 없이 먹을 수 있습니다.\n\n",
-  },
-  {
-    id: "nickname",
-    lastModified: "2024-02-17T11:25:00.000Z",
-    description:
-      "게임에서 눈에 띄는 최고의 닉네임 추천. 다양한 게임 캐릭터와 스타일에 맞는 독특하고 기억에 남는 닉네임을 찾아보세요.",
-    title: "게임에서 눈에 띄는 최고의 닉네임 정하는 방법",
-    image:
-      "https://dust-satin-8b0.notion.site/image/https%3A%2F%2Franking-place.com%2F_next%2Fstatic%2Fmedia%2Fmain.f5bb7950.png?table=block&id=1713a9cb-c73e-4d70-a07a-d45dfb4747c1&spaceId=92d37f21-1548-474e-a4bb-a1e63f34174e&width=2000&userId=&cache=v2",
-    content:
-      "\n![main.f5bb7950.png](https://ranking-place.com/_next/static/media/main.f5bb7950.png)\n\n\n게임 세계 속에서 닉네임은 단순한 명칭 이상의 의미를 지닙니다. 그것은 당신의 캐릭터, 스타일, 심지어 게임 내에서의 정체성까지 표현하는 열쇠이죠. 전사에서 마법사, 신비한 생물에 이르기까지, 각각의 역할에 걸맞은 독특하고 기억에 남는 닉네임을 선택하는 것은 게임 내에서의 당신만의 존재감을 드러내고, 다른 플레이어들과의 소통을 향상시키는 중요한 방법입니다. 2023년, 게임에서 당신만의 정체성을 찾아 나서는 이 여정에서, 이 닉네임들이 당신을 더욱 돋보이게 할 것입니다. 다양한 게임 장르와 선호도에 맞춰 제공되는 이 닉네임들로 게임 세계에서 당신의 이야기를 만들어보세요.\n\n\n## **재미와 유머를 강조한 닉네임:**\n\n\n게임의 세계는 때로는 진지함보다 웃음과 즐거움을 선사하는 곳이죠. 그렇기 때문에, 유머가 담긴 닉네임은 게임 내에서 당신을 더욱 돋보이게 만들어요. 예를 들어, '백설공주'에서 영감을 받아 '뱃살공주'로, 혹은 '짱구는 못말려'를 '짱구는 목말려'로 바꿔, 게임 속에서 유쾌한 분위기의 주인공이 될 수 있어요. 이런 닉네임은 게임을 하는 동안 당신의 존재를 더욱 빛나게 해주며, 다른 플레이어들과의 교류에서도 긍정적인 인상을 남길 거예요.\n\n\n## **커플 닉네임**:\n\n\n게임을 더욱 특별하게 만들고 싶은 커플이라면, 서로를 연결짓는 닉네임을 선택하는 것이 좋아요. 예를 들어, '달과별', '해와달'과 같이 서로의 존재를 상징하는 닉네임은 게임 속에서 여러분의 관계를 더욱 돋보이게 할 거예요. 게임 속에서 여러분만의 사랑을 표현하며, 다른 플레이어들에게도 잊을 수 없는 인상을 남길 수 있겠죠.\n\n\n## **두 글자 닉네임**:\n\n\n간결함 속에서도 강렬한 인상을 남기고 싶다면, 두 글자 닉네임을 선택하는 것이 좋습니다. '천사', '바람' 같은 단어는 기억하기 쉬우면서도 강한 인상을 남겨, 게임 내에서 당신을 쉽게 식별할 수 있게 해줘요. 이런 닉네임은 게임 속에서 당신의 캐릭터를 더욱 독특하게 만들어주며, 당신의 게임 스타일을 반영할 수 있어요.\n\n\n## **당신만의 이야기를 담은 닉네임:**\n\n\n 자신만의 취향, 성격, 게임 속 역할을 반영한 닉네임은 당신이 누구인지, 무엇을 좋아하는지를 세상에 알리는 방법이에요. 예를 들어, '밤하늘의별'은 조용히 밤하늘을 바라보는 것을 좋아하는 당신의 성향을, '화염의마법사'는 게임 속에서 마법사 캐릭터를 선호하는 당신의 취향을 반영할 수 있어요.\n\n\n## **암호화된 메시지:**\n\n\n 자신만이 알 수 있는 숨겨진 의미나 메시지를 닉네임에 포함시키는 것도 재미있는 방법이에요. 예를 들어, 중요한 날짜나 숫자, 기호를 혼합하여 'X7J24'나 'N3oStar' 같은 닉네임을 만들 수 있죠.\n\n\n## **자연 요소:**\n\n\n 자연에서 영감을 받은 닉네임도 멋진 선택이 될 수 있어요. '폭풍의속삭임', '바다의왕자'와 같이 자연의 웅장함과 아름다움을 반영하는 이름들이죠.\n\n\n## **개인화된 닉네임:**\n\n\n당신만의 독특한 닉네임을 원한다면, 자신의 취미, 성격, 혹은 게임 내에서 선호하는 역할을 반영한 닉네임을 고려해보세요. 이 방법은 당신의 게임 내 캐릭터를 더욱 개성있게 만들어 줄 거예요. 예를 들어, 당신이 게임 내에서 전사를 주로 선택한다면, '전투의신 정상수'이라는 닉네임을 선택할 수 있겠죠. 당신의 게임 플레이 스타일과 성격을 반영한 닉네임은 게임 속에서 당신을 더욱 독특하게 만들어 줄 것입니다.\n\n\n이렇게 닉네임을 고르는 과정은 당신의 게임 내 존재를 정의하는 중요한 단계에요. 자신만의 개성과 스타일을 반영한 닉네임을 선택하여, 게임 세계 속에서 독특한 정체성을 구축해보세요!\n\n",
   },
 ] as const;
 
