e82ea65ab41356edb3e1d4c8d96013be6775a213
김태완 <kimtaewan@gimtaewan-ui-MacBookPro.local>
2024-02-17 21:44:11 +0900

    commit

--- src/components/post/recommendPosts/index.tsx
+++ src/components/post/recommendPosts/index.tsx
@@ -16,11 +16,11 @@ export const RecommendPosts = ({ lang }: IProps) => {
   return (
     <aside className="max-w-[1140px] w-full px-3 xl:px-2">
       <p className="my-4 text-left w-full max-w-full text-xl font-bold xl:w-[560px] mx-auto">{local("추천 아티클")}</p>
-      <div className="flex flex-wrap xl:flex-col gap-4 justify-start items-center">
+      <div className="flex flex-wrap xl:flex-col gap-6 justify-start items-center">
         {recommendPost(lang).map(({ title, description, lastModified, image }) => (
           <article className="max-w-full w-[260px] xl:w-[560px] flex flex-col xl:flex-row gap-4" key={lastModified}>
             <Image
-              className=" bg-black  h-[326px] sm:h-[200px] w-[350px] max-[350px] sm:w-[180px] xl:w-full object-cover rounded-sm"
+              className="  bg-black h-[326px] w-[260px] sm:h-[200px] max-w-[350px] sm:w-[180px] object-cover rounded-sm"
               src={image}
               width={260}
               height={326}
