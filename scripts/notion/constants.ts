10bca4634a780133e5bff508a980b0729f48030e
eliot-liner <eliot@linercorp.com>
2024-02-17 20:42:00 +0900

    commit

--- scripts/notion/constants.ts
+++ scripts/notion/constants.ts
@@ -1,5 +1,5 @@
 export const NOTION = {
-  DATABASE_ID: "7ae88a0eb8664fc783481d8e037bddf1",
-  EN_DATABASE_ID: "87f1464bd0c24bca989e04b18d52a949",
+  DATABASE_ID: "dec9645949284e0ba29c891d0acbb06c",
+  EN_DATABASE_ID: "8728d2f2ad254ad78e524bb611aaf1bb",
   AUTH_KEY: "secret_tfWsVm7v3qv8mAuXNbltK1exd0Mjvyjy2lC4WAQyDyq",
 } as const;
